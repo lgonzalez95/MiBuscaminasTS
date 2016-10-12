f1623042a67a32b2812596d47eec6808c275d921
Luis Gonzalez <luis0270@gmail.com>
2016-10-12 07:23:46 -0600

    Juego listo

--- Juego.ts
+++ Juego.ts
@@ -1,31 +1,31 @@
 module BuscaMinas{
-	export class Juego{
-		private ancho:number;
-		private alto:number;
-		private cantidadMinas:number;
-		private tiempoJuego:number;		
-		private interval:number;
-        private matriz: number[][];
-        private buttons: any;
+    export class Juego{
+        private ancho:number;
+        private alto:number;
+        private cantidadMinas:number;
+        private tiempoJuego:number;        
+        private interval:number;
+        private matriz: Array<Array<number>>;
+        private botones: any;
 
-		constructor(_tiempoInicial:number,_ancho:number, _alto:number, _cantidadMinas:number){
-			this.ancho=_ancho;
-			this.alto=_alto;
-			this.tiempoJuego=_tiempoInicial;
+        constructor(_tiempoInicial:number,_ancho:number, _alto:number, _cantidadMinas:number){
+            this.ancho=_ancho;
+            this.alto=_alto;
+            this.tiempoJuego=_tiempoInicial;
             this.cantidadMinas=_cantidadMinas;
 
-			this.iniciarCronometro();
-			this.dibujarTablero("panelJuego");
+            this.iniciarCronometro();
+            this.dibujarTablero("panelJuego");
             this.generarMinas();
+            this.botones = document.getElementsByName('btn');
+            this.iniciarJuego();        
 
-            this.buttons = document.getElementsByName('btn');
-            this.initializeGame();                     
-		}
-
-		 public iniciarCronometro() {            
+        }
+        public iniciarCronometro() {            
             this.interval = setInterval(() => {
               this.tiempoJuego++;
-              document.getElementById('tiempo').innerHTML = this.tiempoJuego.toString();  
+
+              document.getElementById('tiempo').innerHTML = this.tiempoJuego.toString() + " segundos";  
             }, 1000);
         }
 
@@ -34,7 +34,7 @@ module BuscaMinas{
             div.innerHTML = '';
             for (var fila: number = 0; fila < this.alto; fila++) {
                 for (var columna: number = 0; columna < this.ancho; columna++) {
-                    div.innerHTML += '<input type="button" name="btn" id="' + fila + '_' + columna + '" style="background-color:#9E9E9E; font-weight: bold;  font-size: 16px;  width:50px; height:50px" value=" "/>';                    
+                    div.innerHTML += '<input type="button" name="btn" id="' + fila + '_' + columna + '" style="background-color:#9E9E9E; font-weight: bold;  font-size: 14px;  width:50px; height:50px" value=" "/>';                    
                 }
                 div.innerHTML += '<br/>';
             } 
@@ -44,62 +44,63 @@ module BuscaMinas{
             return Math.floor(Math.random() * (maximo - minimo + 1));
         }
 
-        private iniciarMatriz() {
-            let matriz: number[][];//se usaba var
-            matriz = new Array(this.alto);// Las columnas son arrays
-
-            for (let fila: number = 0; fila < this.alto; fila++) {
-                matriz[fila] = new Array(this.ancho);// Las filas son arrays
-
+        private iniciarMatriz() {                
+            this.matriz = new Array(this.alto);// Las columnas son arrays
+            for (let fila: number = 0; fila < this.matriz.length; fila++) {
+                this.matriz[fila] = new Array(this.ancho);// Las filas son arrays
                 for (let columna: number = 0; columna < this.ancho; columna++) {
-                    matriz[fila][columna] = 0;
+                    this.matriz[fila][columna] = 0;
                 }
             }
-            return matriz;
-        }
-        //DE AQUÍ HACIA ARRIBA SE PUEDE PASAR EL CÓDIGO PERFECTAMENTE
+            return this.matriz;
+        }       
 
 
+        
+        
+        public iniciarJuego():void{
+            for (let contador:number = 0; contador < this.botones.length; contador++) {
+                document.getElementById(this.botones[contador].id).onclick=(evento:any)=>{                    
+                    let idBotonPresionado=evento.target.id.split("_");
+                    let valorMatriz:number=this.matriz[idBotonPresionado[0]][idBotonPresionado[1]];
 
-        public generarMinas():void {
-            let _matriz: number[][] = this.iniciarMatriz();
-            var minesCounter: number = 0;
-            var minasAdyacentes: number;
+                    if(valorMatriz==-1){                        
+                        alert("Has perdido el juego");
+                        location.href = location.href;
+                    }else{
+                        this.expandir(parseInt(idBotonPresionado[0]),parseInt(idBotonPresionado[1]),this.matriz);
+                    }
 
-            while (minesCounter < this.cantidadMinas) {                
-                var minaAleatoria: number = this.numAleatorio(0, 1);        //Establece la variable en 0 ó 1           
-                //Saca un vector con 2 valores, uno la fila ubicado en posición 0, y la columna en posicion 1                
-                var posicionAleatoria: number[] = [this.numAleatorio(0, this.alto - 1), this.numAleatorio(0, this.ancho - 1)];                
-                                
-
-                if (!_matriz[posicionAleatoria[0]][posicionAleatoria[1]]) {//Siempre entra al if                                      
-                    minesCounter += (minaAleatoria) ? 1 : 0;    //Si la variable minaAleatoria es 1, suma 1 sino 0                    
-                    if (minaAleatoria) {//si es true O SEA 1, ENTRA                                                
-                         var div = document.getElementById(posicionAleatoria[0] + '_' + posicionAleatoria[1]);
-                         div.style.background = "red";
-                        _matriz[posicionAleatoria[0]][posicionAleatoria[1]] = -1;
-                       
-                        for (var fila: number = posicionAleatoria[0] - 1; fila <= posicionAleatoria[0] + 1; fila++) {                                                        
-                            for (var columna: number = posicionAleatoria[1] -1 ; columna <= posicionAleatoria[1]+1; columna++) {
-                                try {
-                                    minasAdyacentes = _matriz[fila][columna];
-
-                                    if (_matriz[fila][columna] != -1 ) {
-                                        minasAdyacentes += 1;                                        
-                                        _matriz[fila][columna] = minasAdyacentes;
-                                    }                                    
-                                } catch (e) {                                    
-                                }
-                            }
-                        }
-                        
+                    if(this.comprobarGanador(this.ancho,this.alto,this.cantidadMinas)){
+                        alert("Felicidades, has ganado el juego");
+                        location.href = location.href;
                     }
                 }
             }
-            this.matriz = _matriz;
-        } 
+        }
 
-        
+        private comprobarGanador(ancho:number,alto:number,minas:number):boolean{
+            let botonesDisponibles:number=0;
+            let boton:any;
+
+            for (let fila:number = 0; fila < alto; fila++) {
+                for (let columna:number = 0; columna < ancho; columna++) {
+                    boton=document.getElementById( fila+ "_" + columna);
+                    
+                    if(!boton.disabled){
+                        botonesDisponibles++;
+                    }               
+                }                
+                
+            }
+            console.log("Botones disponibles " + botonesDisponibles);
+            if(botonesDisponibles==minas){
+                return true;
+            }else{
+                return false;
+            }            
+        }
+
         private expandir(fila: number, columna: number, matrix: any):boolean {
             
             var btn: any;
@@ -110,14 +111,14 @@ module BuscaMinas{
                 return false;
             }
 
-            if (value!=0) {// si hay más de 0 minas  al boton                
+            if (value!=0) {
                 btn.value = value;
                 btn.style.background = "#EEEEEE";
                 btn.disabled = 'true';  
                 return false;                            
             }
 
-            if (value==false) { //Si no hay minas               
+            if (value==0) {              
                 btn.value = ' ';
                 btn.disabled = 'true';
                 btn.style.background = "#EEEEEE";
@@ -126,56 +127,52 @@ module BuscaMinas{
             var limitesSeleccion = [[fila - 1, columna], [fila + 1, columna], [fila, columna - 1], [fila, columna + 1], [fila - 1, columna - 1], [fila + 1, columna - 1], [fila - 1, columna + 1], [fila + 1, columna + 1]];
             for (var i: number = 0; i < limitesSeleccion.length; i++) {
                 try {                                                               
-                    if (matrix[limitesSeleccion[i][0]][limitesSeleccion[i][1]] != '*') {                       
+                    if (matrix[limitesSeleccion[i][0]][limitesSeleccion[i][1]] != '-1') {                       
                         this.expandir(limitesSeleccion[i][0], limitesSeleccion[i][1], matrix);
                     }
                 } catch (e) { }
             }
-        } 
+        }   
 
-    public comprobarGanador(width: number, height: number, minesNumber: number):boolean {
-            var botonesDisponibles = 0;
-            var btn: any;
+        private generarMinas():void{
+            let nuevaMatriz:number[][]=this.iniciarMatriz();
+            let contadorDeMinas: number=0;
+            let minasCerca:number;            
+            let minaAleatoria:number;
+            let posicionAleatoria:number[];    
 
-            for (var fila = 0; fila < height; fila++) {
+            while(contadorDeMinas <this.cantidadMinas){                                                        
+                minaAleatoria=this.numAleatorio(0,1);                    
+                posicionAleatoria=[this.numAleatorio(0,this.alto-1),this.numAleatorio(0,this.ancho-1)];                                                            
+                if(nuevaMatriz[posicionAleatoria[0]][posicionAleatoria[1]]!=-1){                                   
+                    if(minaAleatoria!=0){
+                        contadorDeMinas++;                                                  
+                        let div = document.getElementById(posicionAleatoria[0]+"_"+posicionAleatoria[1]);
+                        div.style.background="red";
+                        nuevaMatriz[posicionAleatoria[0]][posicionAleatoria[1]]=-1;                        
+                        
+                         for (let fila:number=posicionAleatoria[0]-1; fila <=posicionAleatoria[0]+1; fila++) {                             
+                            for (let columna:number = posicionAleatoria[1]-1; columna <=posicionAleatoria[1]+1; columna++) {                               
+                                try{                                                                                                           
+                                       minasCerca=nuevaMatriz[fila][columna];                                       
+                                       if(nuevaMatriz[fila][columna] != -1){                                       
+                                       minasCerca++;                                                                          
+                                       nuevaMatriz[fila][columna]=minasCerca;                                                                     
+                                    } 
 
-                for (var columna = 0; columna < width; columna++) {
-                    btn = document.getElementById(fila + '_' + columna);
+                                }catch(e){}                                
+                            }
+                        }
 
-                    if (!btn.disabled) {
-                        botonesDisponibles++;
                     }
-                }
+                    
+                }               
             }
-            return botonesDisponibles == minesNumber;
-        }
-
-        private initializeGame() {            
-            for (var i: number = 0; i < this.buttons.length; i++) {
-                document.getElementById(this.buttons[i].id).onclick = (evento: any) => {                    
-                    var idBotonPresionado = evento.target.id.split('_');// Obtiene el id del evento que lo dispara
-                                                                        //Eliminando el _                                        
-                    var value = this.matriz[parseInt(idBotonPresionado[0])][parseInt(idBotonPresionado[1])];
-
-                    if (this.matriz[parseInt(idBotonPresionado[0])][parseInt(idBotonPresionado[1])] == -1) {
-                        evento.target.value = '*';
-                        alert('Has perdido!');
-                        location.href = location.href;
-                    } else {                        
-                        this.expandir(parseInt(idBotonPresionado[0]), parseInt(idBotonPresionado[1]), this.matriz);
-                    }
-
-                    if (this.comprobarGanador(this.ancho, this.alto, this.cantidadMinas)==true) {
-                        alert('Felicidades, Has ganado el juego');
-                        location.href = location.href;
-                    }
-                }
-            }
-        }
-
-	}
+            this.matriz=nuevaMatriz;
+        }          
+    }
 }
 
 window.onload = () => {
-    var juego = new BuscaMinas.Juego(0,6, 6,5);
+    var juego = new BuscaMinas.Juego(0,6, 6,10);
 };
